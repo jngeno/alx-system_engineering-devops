976c0cf63ac5c08f9ef1e088aa23fd65ae1b0fff
jngeno <johnngeno.jn@gmail.com>
2023-04-25 16:18:59 +0300

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# Install Flask package
-package { 'Flask':
-  ensure => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  creates => '/usr/local/lib/python3.6/dist-packages/Flask-2.1.0.dist-info',
 }
